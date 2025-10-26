6406a8d4bbb5c747d9a7a63467cf62dea47b6dcc
bilal-alaabadi <mdbilalnizwa@gmail.com>
2025-10-26 13:48:12 +0400

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin:"https://sundal-f.vercel.app",
+        origin:"https://alsandalbeauty.com",
         // origin: "https://www.royasow.store",//مال الفرونت اند
         credentials: true,
     })
@@ -23,7 +23,7 @@ app.use(
 
 // دعم طلبات OPTIONS (Preflight Requests)
 app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'https://sundal-f.vercel.app');
+    res.header('Access-Control-Allow-Origin', 'https://alsandalbeauty.com');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.send();
